0c4ef268686e93ac1dccaff4f3f67b8956195687
me <mfeklis@gmail.com>
2019-01-31 21:47:08 +0200

    Added change color used answers

--- play.js
+++ play.js
@@ -5,17 +5,19 @@ var isWrong = document.getElementsByClassName('isWrong');
 var sumOfAnswers = 0;
 var sumOfWrongAnswers = 0;
 
-function handler1() {
+function handler1(event) {
   sumOfAnswers++;
   isCorrect.textContent = sumOfAnswers;
+  event.target.style.color="green";
 };
-function handler2() {
+function handler2(event) {
   sumOfWrongAnswers++;
   isNotCorrect.textContent = sumOfWrongAnswers;
+  event.target.style.color="red";
 };
 for (var i = 0 ; i < isTrue.length; i++) {
-  isTrue[i].addEventListener('click', handler1, {once: true} );
+  isTrue[i].addEventListener('click', handler1.bind(event), {once: true} );
 };
 for (var i = 0 ; i < isWrong.length; i++) {
-  isWrong[i].addEventListener('click', handler2, {once: true} );
+  isWrong[i].addEventListener('click', handler2.bind(event), {once: true} );
 };
