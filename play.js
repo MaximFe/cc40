cd59e07eefb43f5bd522d30eb44c686ad2e4a7e3
me <mfeklis@gmail.com>
2019-02-01 17:26:25 +0200

    Deleted soundClick()

--- play.js
+++ play.js
@@ -5,22 +5,15 @@ var isWrong = document.getElementsByClassName('isWrong');
 var sumOfAnswers = 0;
 var sumOfWrongAnswers = 0;
 
-function soundClick() {
-  var audio = new Audio();
-  audio.src = 'sound/click.mp3';
-  audio.autoplay = true;
-}
 function handler1(event) {
   sumOfAnswers++;
   isCorrect.textContent = sumOfAnswers;
   event.target.style.color="green";
-  soundClick();
 };
 function handler2(event) {
   sumOfWrongAnswers++;
   isNotCorrect.textContent = sumOfWrongAnswers;
   event.target.style.color="red";
-  soundClick();
 };
 for (var i = 0 ; i < isTrue.length; i++) {
   isTrue[i].addEventListener('click', handler1.bind(event), {once: true} );
